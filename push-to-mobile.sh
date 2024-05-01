807001ce56e9443cc6d1d1ebab5458a24c7372ba
pukepail <168659749+pukepail@users.noreply.github.com>
2024-05-01 18:42:23 +0200

    Update push-to-mobile.sh

--- push-to-mobile.sh
+++ push-to-mobile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip -r "disable-javascript@pacassi.ch.xpi" . -x "*.git*" "*.sh" "*.idea*" "*.DS_Store" "*node_modules*" "pages/src/*"
-$ANDROID_HOME/platform-tools/adb push "disable-javascript@pacassi.ch.xpi" /mnt/sdcard/
+zip -r "control-javascript@pukepail.xpi" . -x "*.git*" "*.sh" "*.idea*" "*.DS_Store" "*node_modules*" "pages/src/*"
+$ANDROID_HOME/platform-tools/adb push "control-javascript@pukepail.xpi" /mnt/sdcard/
